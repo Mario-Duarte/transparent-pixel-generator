4dd879632e5c8fbc7716e392ea2006aae7c2fce8
Mario-Duarte <djtsunamimario@gmail.com>
2025-08-02 00:04:55 +0100

    adding icon library

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { CoCopy } from "oh-vue-icons/icons";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.component("v-icon", OhVueIcon);
+addIcons(CoCopy);
+app.mount("#app");
